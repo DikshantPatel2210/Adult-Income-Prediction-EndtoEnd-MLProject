9a08a176b277497f3d0dff4b2c52dae7146146fc
DIkshantPatel2210 <sikshantpatel2210@.com>
2024-11-20 12:12:29 +0100

    create template.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+with open("README.md", "r",encoding = "urf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0"
+SRC_REPO = "AdultIncomePrediction"
+AUTHOR_USER_NAME: "DIKSHANT PATEL"
+REPO_NAME: "ADULT_INCOME_PREDICTION_EndtoEnd"
+setuptools.setup()
